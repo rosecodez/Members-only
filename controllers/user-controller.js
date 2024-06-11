61e4e7c070773608cff1ae8cb4c6702f4e0e7ea7
Rose <rosebeats09@gmail.com>
2024-06-11 18:26:37 -0400

    Create navbar for layout

---
+++ controllers/user-controller.js
@@ -0,0 +1,3 @@
+const User = require("../models/user");
+const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
--- routes/members-only.js
+++ routes/members-only.js
@@ -3,3 +3,9 @@ const router = express.Router;
 
 const userController = require("../controllers/user-controller");
 const messageController = require("../controllers/message-controller");
+
+// sign-up form
+router.get("/members-only/sign-up", userController.user_create_get);
+router.post("/members-only/sign-up", userController.user_create_post);
+
+// login form
